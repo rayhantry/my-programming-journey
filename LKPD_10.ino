42a5774c06299b4de9bd7190704173e8c26b8157
rayhantry <rtea1351@gmail.com>
2024-11-12 15:36:44 +0700

    progam pusbuton serial monitor

---
+++ LKPD_10.ino
@@ -0,0 +1,16 @@
+//@pakgurud9
+//XII TAV 4
+const int buttonPin = 26;
+void setup() {
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  int buttonState = digitalRead(buttonPin);
+  Serial.print("Tombol ditekan: ");
+  if (buttonState == HIGH) {
+    Serial.println("YA"); 
+  } else {
+    Serial.println("TIDAK");
+  delay(100);
+}
